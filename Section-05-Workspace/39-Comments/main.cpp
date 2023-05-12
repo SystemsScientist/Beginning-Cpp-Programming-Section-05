7eb690d7ef1566624993b38529d8184f294df54d
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-12 06:08:10 -0500

    Adding 39-Comments files and directories

---
+++ Section-05-Workspace/39-Comments/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Author: Matt Johnson
+*  Subject: Comments
+*  Purpose:
+*  Date: 10/12/2023 
+* 
+*  Notes:
+* 
+*       - This is a multi-line comment
+*       - Are programmer explanations embedded in source code
+*       - They are also notes, annotations, and anything the
+*          program is doing
+ * 
+ */
+
+#include <iostream>
+
+int main(void) {
+
+    int favorite_number; // this is a single line comment with a variable
+    
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number; // here is another single line comment
+    
+    std::cout << "Amazing! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    
+    return 0;
+}
