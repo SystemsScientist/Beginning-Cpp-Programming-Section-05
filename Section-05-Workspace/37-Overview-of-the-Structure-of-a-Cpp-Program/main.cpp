402bf667443f573881ebf6efe1aac05c76cc9182
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-12 05:39:47 -0500

    Adding Section-05-Workspace files and directories

---
+++ Section-05-Workspace/37-Overview-of-the-Structure-of-a-Cpp-Program/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Author: Matt Johnson
+*  Subject: C++ Keywords
+*  Purpose:
+*  Date: 10/12/2023 
+* 
+*  Notes:
+* 
+*       Identifiers - main, favorite_number, cout, cin
+*       Keywords - int, return
+ * 
+ */
+
+#include <iostream>
+
+int main(void) {
+    
+    int favorite_number;
+    
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    
+    std::cout << "Amazing! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    
+    return 0;
+}
