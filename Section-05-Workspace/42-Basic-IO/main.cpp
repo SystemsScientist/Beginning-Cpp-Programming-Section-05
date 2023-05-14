bb3d0824fd654890da4ce3681f4f60389a49cf20
Matthew Johnson <matthewjohnson@snarkydata.com>
2023-05-14 06:41:16 -0500

    Adding 42-Basic-IO files and directories

---
+++ Section-05-Workspace/42-Basic-IO/main.cpp
@@ -0,0 +1,63 @@
+/*
+*  Author: Matt Johnson
+*  Subject: Basic Input and Output
+*  Purpose:
+*  Date: 05/14/2023 
+* 
+*  Notes:
+* 
+*       - Uncomment to compile, build, and execute code
+*
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+//    cout << "Hello, World!" << endl;
+    
+//     cout << "Hello";
+//     cout << "World" << endl;
+    
+//     cout << "Hello World!" << endl;
+//     cout << "Hello" << " world!" << endl;
+//     cout << "Hello" << " world!\n";
+//     cout << "Hello\nOut\nThere\n";
+     
+     int num1;
+     int num2;
+     double num3;
+//     
+//     cout << "Enter an integer: ";
+//     cin >> num1;
+//     cout << "You entered: " << num1 << endl;
+
+//    cout << "Enter a first integer: ";
+//    cin >> num1;
+//    
+//    cout << "Enter a second integer: " ;
+//    cin >> num2;
+    
+//    cout << "You entered " << num1 << " and " << num2 << endl;
+    
+//    cout << "Enter 2 integers separated with a space: ";
+//    cin >> num1 >> num2;
+//    cout << "You entered " << num1 << " and " << num2 << endl;
+    
+//    cout << "Enter a double: ";
+//    cin >> num3;
+//    cout << "You entered : " << num3 << endl;
+    
+    cout << "Enter an integer: ";
+    cin >> num1;
+    
+    cout << "Enter a double: ";
+    cin >> num3;
+    
+    cout << "The integer is: " << num1 << endl;
+    cout << "And the double is " << num3 << endl;
+    
+    return 0;
+}
